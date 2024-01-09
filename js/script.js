0663e351f43e884b0baf818907974bfc6ce040b8
Rihan385 <rihanmojib386@gmail.com>
2024-01-09 20:39:18 +0600

    add responsive design

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let search = document.getElementById("search")
+let business_law = document.getElementById("business_law")
+let criminal_law = document.getElementById("criminal_law")
+let child_support = document.getElementById("child_support")
+let education_law = document.getElementById("education_law")
+let divorce_law = document.getElementById("divorce_law")
+let tax_law = document.getElementById("tax_law")
+
